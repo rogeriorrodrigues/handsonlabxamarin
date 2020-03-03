c12d1ae14b326350ab337da2afb2d1661e88c904
Logon PF Fiap <logonpflocal@fiap.com.br>
2020-03-02 21:02:33 -0300

    correcao hands on baseviewmodel

--- Xamarin.Forms/Start/MonkeyFinder/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Xamarin.Forms/Start/MonkeyFinder/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -4,11 +4,46 @@ using Xamarin.Forms;
 
 namespace MonkeyFinder.ViewModel
 {
-    public class BaseViewModel
+    public class BaseViewModel : INotifyPropertyChanged
     {
+        bool isBusy;
+        string title;
         public BaseViewModel()
         {
-            
+
+
         }
+
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                if (isBusy == value)
+                    return;
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title == value)
+                    return;
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        private void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
+
+
 }
